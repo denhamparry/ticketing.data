90b3e5d903bd6f63e0486ba332153607be5799de
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-09 17:48:20 +0000

    Create prepop data in Dockerfile

--- MongoDB/startup.sh
+++ MongoDB/startup.sh
@@ -1,1 +1,12 @@
-mkdir bin/data
+#!/bin/bash
+# Our custom function
+prepop_func(){
+  sleep 5
+  mongo --eval 'mongo'
+  mongo --eval 'use TicketingDb'
+  mongo --eval 'db.createCollection("Tickets")'
+  mongo --eval 'db.Tickets.insertMany([{"Name":"Ellie","Browser":"Safari","Category":"Talk","Speaker":"Lewis Denham-Parry"}, {"Name":"Laurie","Browser":"Chrome","Category":"Talk","Speaker":"Lewis Denham-Parry"}])'
+  mongo --eval 'db.Tickets.find({}).pretty()'
+}
+
+prepop_func $i & mongod
